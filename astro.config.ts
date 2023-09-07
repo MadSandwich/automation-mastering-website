d4a5e53e8b9fb93291c2385ae26d077e8c810f2f
Artsem Burlai <57759145+MadSandwich@users.noreply.github.com>
2023-09-07 11:23:04 +0300

    prettier style

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
 
 export default defineConfig({
 	site: 'https:localhost:4321',
 	output: 'hybrid',
-  integrations: [tailwind()]
-});
+	integrations: [tailwind()],
+})
